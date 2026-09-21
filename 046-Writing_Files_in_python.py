aa9e172ca91b9fa54d1e43fb6d9d82459543b445
Muhammad Nouman Safeer <rajanoumac8@gmail.com>
2026-09-21 10:13:15 +0500

    Update 046-Writing_Files_in_python.py

--- 046-Writing_Files_in_python.py
+++ 046-Writing_Files_in_python.py
@@ -1,5 +1,14 @@
+#####Read File
 file=open("doc.txt" , "r")
 print(file.read())
+
+#####Over-write the text in file 
 file=open("doc.txt" , "w")
 file.write("I'm A Good Boy")
+
+#####File closing
 file.close()
+
+#####Append data into files
+file=open("doc.txt" ,  "a")
+file.write("\nNo I'm Not a Good Boy")
